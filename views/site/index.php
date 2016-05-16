c361591904cc5f1e768476078fe7231fff17b172
mefiac <mefiac@mail.ru>
2016-05-16 19:53:55 +0300

    передвижения блока

---
+++ views/site/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <div class="well">
+            <p>Фигура <select id="FigureAdd">
+                    <option value="1">пешка</option>
+                    <option value="2">ферзь</option>
+                    <option value="3">король</option>
+                </select></p>
+            <button class="btn" id="ButtonAdd">Добавить фигуру</button>
+        </div>
+
+      <table class="" border="1" id="tab">
+          <tr> <td> <div id="12" class="elem" style="width: 50px;height: 50px; background: #DDE0E0;"></div></td><td></td><td></td></tr>
+          <tr><td></td><td></td><td></td></tr>
+          <tr><td></td><td></td><td></td></tr>
+      </table>
+    </div>
+
+<script type="text/javascript">
+
+</script>
+</div>
---
+++ web/js/main.js
@@ -0,0 +1,37 @@
+﻿$(".elem").draggable({
+    containment: 'parent'
+});
+$(document).ready(function () {
+    $('td').each(function () {
+        $(this).click(function () {
+
+            if($(this).find('.activElem').length==0){
+            var elem=$('.activElem');
+                elem .
+                clone().
+                addClass('activElem').
+                removeAttr('id').
+                attr('id', token()).prependTo($(this));
+                elem.detach();
+
+        }
+
+        });
+
+
+    });
+    $('.elem').click(function (event) {
+
+        var id = event.target.id;
+            $(".elem").removeClass('activElem');
+            $('#' + id).addClass('activElem');
+
+    });
+    var rand = function () {
+        return Math.random().toString(36).substr(2);
+    };
+
+    var token = function () {
+        return rand() + rand();
+    };
+});
